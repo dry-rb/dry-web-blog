82ae44441b6bdccf8024088ad6de9f84fdee0d08
Tim Riley <tim@openmonkey.com>
2017-07-17 23:13:20 +1000

    Make default article factory attributes strings, not arrays

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,7 @@
 Factory.define :article do |f|
   f.sequence(:slug) { |i| "article-#{i}" }
-  f.title { fake(:lorem, :words, 5) }
-  f.body { fake(:lorem, :paragraphs, 1) }
+  f.title { fake(:lorem, :words, 5).join(" ") }
+  f.body { fake(:lorem, :paragraphs, 1).join("\n\n") }
   f.published true
   f.published_at { Time.now }
   f.timestamps
