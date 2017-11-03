6859e1023ab45e5cfc5d7a798f620fa8521505d8
Tim Riley <tim@openmonkey.com>
2017-11-03 22:28:45 +1300

    Update bootable components for dry-system 0.8
    - Rename `rom` component to `persistence` so it can be booted automatically when resolved from the container
    - Update component `#boot`/`boot!` calls to `#init`/`#start`

--- spec/db_spec_helper.rb
+++ spec/db_spec_helper.rb
@@ -1,6 +1,6 @@
 require_relative "spec_helper"
 
-Blog::Container.boot! :rom
+Blog::Container.start :persistence
 
 Dir[SPEC_ROOT.join("support/db/*.rb").to_s].each(&method(:require))
 Dir[SPEC_ROOT.join("shared/db/*.rb").to_s].each(&method(:require))
